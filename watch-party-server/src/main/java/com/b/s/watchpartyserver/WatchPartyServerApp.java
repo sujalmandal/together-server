e85f9e543f141b6e2435f6cca568acd4d26033c4
Sujal <ss6sujal@gmail.com>
2021-03-17 10:20:32 +0530

    initial commit

---
+++ watch-party-server/src/main/java/com/b/s/watchpartyserver/WatchPartyServerApp.java
@@ -0,0 +1,12 @@
+package com.b.s.watchpartyserver;
+
+public final class WatchPartyServerApp {
+
+    private WatchPartyServerApp() {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
